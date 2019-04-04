bc2a80eeb83271de49f9a64ddd6fed0a5c031b19
Faurepeq <39625490+Faurepeq@users.noreply.github.com>
2019-04-05 01:33:29 +0200

    jhj

--- model.js
+++ model.js
@@ -1,14 +1,14 @@
-var model = {};
-model.recherche_courante="";
-model.recherches=[];
-model.recherche_courante_news=[];
-
-model.setRechercheCourante = function(recherche)
-{
-  this.recherche_courante = recherche;
-}
-
-model.getRechercheCourante = function()
-{
-  return this.recherche_courante;
-}
+// var model = {};
+// model.recherche_courante="";
+// model.recherches=[];
+// model.recherche_courante_news=[];
+//
+// model.setRechercheCourante = function(recherche)
+// {
+//   this.recherche_courante = recherche;
+// }
+//
+// model.getRechercheCourante = function()
+// {
+//   return this.recherche_courante;
+// }
--- vue.js
+++ vue.js
@@ -1,40 +1,40 @@
-var view ={};
-
-
-view.setAttribute = function(element,attribut,valeur){
-  element.setAttribute(attribut,valeur);
-}
-
-
-//retourne la value de la zone de saisie
-view.getValZoneSaisie = function(){
-  return document.getElementById("zone_saisie").value;
-}
-
-
-//set la value dans la zone de saisie
-view.setValZoneSaisie = function(value){
-  document.getElementById("zone_saisie").value = value;
-}
-
-
-view.creerRecherche = function(sauvegarde,id){
-  //creation d'un element p
-  var recherches = document.createElement("p");
-  var	recherches.className = 'titre-recherche';
-  //création d'un élément label
-  var label = document.createElement("label");
-  label.innerHTML =  sauvegarde;
-  label.setAttribute("onclick","controler.selectionner_recherche(this)");
-  label.setAttribute("id",id);
-  // change le nom de la recherche stockée
-  label.setAttribute("ondblclick","controler.modifIdRecherche(this)");
-  recherches.append(label);
-  //création d'un élément img pour supprimer une recherche
-  var effacer = document.createElement("img");
-  effacer.src = 'croix30.jpg';
-  effacer.className = 'icone-croix';
-  effacer.setAttribute("onclick","controler.supprimer_recherche(this)");
-  recherches.append(effacer);
-  document.getElementById("recherches-stockees").append(p);
-}
+// var view ={};
+//
+//
+// view.setAttribute = function(element,attribut,valeur){
+//   element.setAttribute(attribut,valeur);
+// }
+//
+//
+// //retourne la value de la zone de saisie
+// view.getValZoneSaisie = function(){
+//   return document.getElementById("zone_saisie").value;
+// }
+//
+//
+// //set la value dans la zone de saisie
+// view.setValZoneSaisie = function(value){
+//   document.getElementById("zone_saisie").value = value;
+// }
+//
+//
+// view.creerRecherche = function(sauvegarde,id){
+//   //creation d'un element p
+//   var recherches = document.createElement("p");
+//   var	recherches.className = 'titre-recherche';
+//   //création d'un élément label
+//   var label = document.createElement("label");
+//   label.innerHTML =  sauvegarde;
+//   label.setAttribute("onclick","controler.selectionner_recherche(this)");
+//   label.setAttribute("id",id);
+//   // change le nom de la recherche stockée
+//   label.setAttribute("ondblclick","controler.modifIdRecherche(this)");
+//   recherches.append(label);
+//   //création d'un élément img pour supprimer une recherche
+//   var effacer = document.createElement("img");
+//   effacer.src = 'croix30.jpg';
+//   effacer.className = 'icone-croix';
+//   effacer.setAttribute("onclick","controler.supprimer_recherche(this)");
+//   recherches.append(effacer);
+//   document.getElementById("recherches-stockees").append(p);
+// }
